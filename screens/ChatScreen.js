4f8d882cb0a79de664d7c5d635a67a25bcf9a3f4
Erica Carballo <airwreckuh101@gmail.com>
2021-07-20 11:37:16 -0700

    added code from notion into app.js, chatscreen.js, and homescreen.js

---
+++ screens/ChatScreen.js
@@ -0,0 +1,50 @@
+import React, { useState, useCallback, useEffect } from "react";
+import { GiftedChat } from "react-native-gifted-chat";
+import db from "./firebase";
+import firebase from "firebase/app";
+
+export default function ChatScreen({ navigation }) {
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    let unsubscribeFromNewSnapshots = db
+      .collection("Chats")
+      .doc("myfirstchat")
+      .onSnapshot((snapshot) => {
+        console.log("New Snapshot!");
+        setMessages(snapshot.data().messages);
+      });
+
+    return function cleanupBeforeUnmounting() {
+      unsubscribeFromNewSnapshots();
+    };
+  }, []);
+
+  const onSend = useCallback((messages = []) => {
+    db.collection("Chats")
+      .doc("myfirstchat")
+      .update({
+        // arrayUnion appends the message to the existing array
+        messages: firebase.firestore.FieldValue.arrayUnion(messages[0]),
+      });
+    setMessages((previousMessages) =>
+      GiftedChat.append(previousMessages, messages)
+    );
+  }, []);
+
+  return (
+    <GiftedChat
+      messages={messages}
+      onSend={(messages) => onSend(messages)}
+      user={{
+        // current "blue bubble" user
+        _id: "1",
+        name: "Ashwin",
+        avatar: "https://placeimg.com/140/140/any",
+      }}
+      inverted={true}
+      showUserAvatar={true}
+      renderUsernameOnMessage={true}
+    />
+  );
+}
---
+++ screens/HomeScreen.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from "react";
+import { FlatList, Text, View, TouchableOpacity, StyleSheet } from "react-native";
+import db from "./firebase";
+
+export default function HomeScreen({ navigation }) {
+  const [chatList, setChatList] = useState([]);
+
+  useEffect(() => {
+    let chatsRef = db.collection("Chats");
+    chatsRef.get().then((querySnapshot) => {
+      let newChatList = [];
+      querySnapshot.forEach((doc) => {
+        let newChat = { ...doc.data() };
+        newChat.id = doc.id;
+        newChatList.push(newChat);
+        console.log(newChatList);
+      });
+      setChatList(newChatList);
+    });
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={chatList}
+        renderItem={({ item }) => (
+          <TouchableOpacity
+            onPress={() => navigation.navigate("Chat")}
+          >
+            <Text style={styles.item}>{item.id}</Text>
+          </TouchableOpacity>
+        )}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+  },
+});
