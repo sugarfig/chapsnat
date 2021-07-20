2a4caad449da3d168d80985223981a516e7e81c4
Erica Carballo <airwreckuh101@gmail.com>
2021-07-20 11:34:16 -0700

    added screens folder and homescreen and chatscreen.js files

--- App.js
+++ App.js
@@ -1,13 +1,79 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState, useCallback, useEffect } from "react";
 import { StyleSheet, Text, View } from 'react-native';
+import { GiftedChat } from 'react-native-gifted-chat';
+import { Video, Audio } from 'expo-av';
+import db from "./firebase";
+import firebase from "firebase/app";
 
 export default function App() {
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    let unsubscribeFromNewSnapshots = db
+      .collection("Chats")
+      .doc("myfirstchat")
+      .onSnapshot((snapshot) => {
+        console.log("New Snapshot!");
+        setMessages(snapshot.data().messages);
+      });
+
+      return function cleanupBeforeUnmounting() {
+        unsubscribeFromNewSnapshots();
+      };
+    }, []);
+
+  console.log(messages);
+  const onSend = useCallback((messages = []) => {
+    db.collection("Chats").doc("myfirstchat").update({ messages: firebase.firestore.FieldValue.arrayUnion(messages[0])});
+  
+    setMessages((previousMessages) =>
+      GiftedChat.append(previousMessages, messages)
+    );
+  }, []);
+
+  // const onSend = useCallback((messages = []) => {
+  //   setMessages((previousMessages) =>
+  //     GiftedChat.append(previousMessages, messages)
+  //   );
+  // }, []);
+
+  // useEffect(() => {
+  //   setMessages([
+  //     {
+  //     db.collection("Chats")
+  //     .doc("myfirstchat")
+  //     .get()
+  //     .then((snapshot) => {
+  //       console.log(snapshot.id);
+  //       console.log(snapshot.data());
+  //       },
+  //     },
+  //   ])
+  // }, [])
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      // <View style={{ padding: 20 }}>
+      //     <Video
+      //      resizeMode="contain"
+      //      useNativeControls
+      //      shouldPlay={false}
+      //      source={{ uri: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'}}
+      //      style={styles.video}
+      //    />
+      // </View>
+      <GiftedChat
+      messages={messages}
+      onSend={messages => onSend(messages)}
+      user = {{_id: 1, name: "Erica", avatar: "https://placeimg.com/140/140/any"}}
+      inverted = {true}
+      showUserAvatar = {true}
+      // placeholder = {"Type something"}
+      // alwaysShowSend = {true}
+      renderUsernameOnMessage = {true}
+      />
+      
   );
 }
 
---
+++ firebase.js
@@ -0,0 +1,21 @@
+
+import firebase from "@firebase/app";
+import "@firebase/auth";
+import "@firebase/firestore";
+
+// Your web app's Firebase configuration, which you copy-pasted from Step 6
+const firebaseConfig = {
+  apiKey: "AIzaSyC7CQwBSzjC_tlEiMd2Mc8Sh9Fb_Cwc1p8",
+  authDomain: "chapsnat-3f4f7.firebaseapp.com",
+  projectId: "chapsnat-3f4f7",
+  storageBucket: "chapsnat-3f4f7.appspot.com",
+  messagingSenderId: "239440555368",
+  appId: "1:239440555368:web:d7d431a3733e778d273add",
+  measurementId: "G-W4Y70B8JL2",
+};
+// Initialize Firebase
+
+firebase.initializeApp(firebaseConfig);
+let firestore = firebase.firestore();
+
+export default firestore;
