6a50c2ba80f1dc3992b7ad69bef1bb6ff54076ff
Erica Carballo <airwreckuh101@gmail.com>
2021-07-21 10:41:57 -0700

    added username and email to profile screen and added code to friends screen

--- screens/FriendsScreen.js
+++ screens/FriendsScreen.js
@@ -1,12 +1,96 @@
 
-import { Text, View } from "react-native";
+import {
+  Text,
+  View,
+  TextInput,
+  Button,
+  StyleSheet,
+  KeyboardAvoidingView,
+} from "react-native";
+import CustomMultiPicker from "react-native-multiple-select-list";
 
 import * as React from "react";
+import { useState, useEffect } from "react";
+import Colors from "../constants/Colors";
+
+const userListDemo = {
+  123: "Jenny",
+  124: "Ashwin",
+  125: "Pavan",
+};
 
 export default function FriendsScreen() {
+  const [chatName, setChatName] = useState("");
+  const [userList, setUserList] = useState([]);
+  const [selectedUsers, setSelectedUsers] = useState([]);
+
+  useEffect(() => {
+    // download user list from Firebase
+  }, []);
+
+  const onPressCreateChat = () => {
+    console.log("Create Chat button pressed!");
+    // Create new chat in Firebase
+  };
+
   return (
     <View>
-      <Text>Friends Screen</Text>
+      <View style={styles.friendListContainer}>
+        <CustomMultiPicker
+          options={userListDemo}
+          search={true} // should show search bar?
+          multiple={true} // allow multiple select
+          placeholder={"Search"}
+          placeholderTextColor={"#757575"}
+          returnValue={"value"} // label or value
+          callback={(selected) => {
+            selected = selected.filter((user) => user !== undefined);
+            setSelectedUsers(selected);
+          }} // callback, array of selected items
+          rowBackgroundColor={"#eee"}
+          rowHeight={45}
+          rowRadius={5}
+          searchIconName="ios-checkmark"
+          searchIconColor="red"
+          searchIconSize={30}
+          iconColor={"#00a2dd"}
+          iconSize={30}
+          selectedIconName={"ios-checkmark-circle-outline"}
+          unselectedIconName={"ios-radio-button-off-outline"}
+          scrollViewHeight={300}
+        />
+      </View>
+      <KeyboardAvoidingView behavior="position">
+        <View style={styles.newChatContainer}>
+          <Text>New chat name:</Text>
+          <TextInput style={styles.chatNameInput} onChangeText={setChatName} />
+          <Button
+            onPress={onPressCreateChat}
+            title="Create Chat"
+            color={Colors.snapblue}
+            accessibilityLabel="Create Chat"
+            disabled={chatName.length < 1 || selectedUsers.length < 1}
+          />
+        </View>
+      </KeyboardAvoidingView>
     </View>
   );
 }
+
+const offset = 16;
+const styles = StyleSheet.create({
+  newChatContainer: {
+    margin: offset,
+    padding: offset,
+    borderRadius: offset,
+    backgroundColor: Colors.snapyellow,
+  },
+  chatNameInput: {
+    height: offset * 2,
+    margin: offset,
+    paddingHorizontal: offset,
+    borderColor: "#111111",
+    borderWidth: 1,
+    fontSize: offset,
+  },
+});
--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,12 +1,97 @@
-
-import { Text, View } from "react-native";
 import firebase from "@firebase/app";
-import * as React from "react";
+import React, { useState } from "react";
+import Colors from "../constants/colors";
+import { Image, StyleSheet, Text, View, TouchableOpacity } from "react-native";
+
 
 export default function ProfileScreen() {
+
+  const onPressLogout = async () => {
+    await firebase.auth().signOut().then(() => {
+      // Sign-out successful.
+      console.log("Signed out");
+    })
+    .catch((error) => {
+      var errorMessage = error.message
+      // An error happened.
+      alert(error.message);
+    });
+  };
   return (
-    <View>
-      <Text>{firebase.auth().currentUser.displayName}</Text>
+    <View style={styles.container}>
+      <View style={styles.headerColumn}>
+        <TouchableOpacity onPress={() => alert("edit!")}>
+          <Image style={styles.userImage} source={{}} />
+        </TouchableOpacity>
+        <Text style={styles.userNameText}>
+        {firebase.auth().currentUser.displayName}
+        </Text>
+        <View style={styles.Row}>
+          <Text style={styles.descriptionText}>{firebase.auth().currentUser.email}</Text>
+        </View>
+      </View>
+      <View style={styles.Row}>
+        <TouchableOpacity
+          style={[styles.buttonContainer, styles.logoutButton]}
+          onPress={onPressLogout}
+        >
+          <Text style={styles.logoutText}>Logout</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+  },
+  headerColumn: {
+    backgroundColor: "transparent",
+    paddingBottom: 20,
+    paddingTop: 45,
+  },
+  Row: {
+    alignItems: "center",
+    flexDirection: "row",
+    justifyContent: "center",
+  },
+  descriptionText: {
+    color: "#A5A5A5",
+    fontSize: 15,
+    fontWeight: "600",
+    textAlign: "center",
+  },
+  userImage: {
+    borderColor: "#FFF",
+    borderRadius: 85,
+    borderWidth: 3,
+    height: 170,
+    marginBottom: 15,
+    width: 170,
+  },
+  userNameText: {
+    color: "black",
+    fontSize: 22,
+    fontWeight: "bold",
+    paddingBottom: 8,
+    textAlign: "center",
+  },
+  buttonContainer: {
+    height: 45,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: 20,
+    width: 300,
+    borderRadius: 30,
+    backgroundColor: "transparent",
+  },
+  logoutButton: {
+    backgroundColor: Colors.snapblue,
+  },
+  logoutText: {
+    color: "white",
+    fontWeight: "bold",
+  },
+});
